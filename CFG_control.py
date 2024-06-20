07a4105abae5a43904adeb8f5e42450de6db4422
Meyson123 <maks.naumenko.420@mail.ru>
2024-06-21 00:09:01 +0700

    Добавлен режим колаба(нейропряники + GTA)

--- CFG_control.py
+++ CFG_control.py
@@ -1,31 +1,39 @@
 import io
 import subprocess
-ai = int(input( 'Выбери конфиг (Циферку выбери)\n'
-                '1) Нейропряны\n'
-                '2) Гта\n'
-                '3) Колаб\n'
-                '4) Ролик\n' ))
-if  ai == 1:
-    with io.open('myConfig.py','w+',encoding='utf-8') as file, io.open('configs/NeuroPryaniky.txt','r',encoding='utf-8') as cfg_file:
+
+ai = int(input('Выбери конфиг:\n'
+               '1) Нейропряны\n'
+               '2) Гта\n'
+               '3) Колаб\n'
+               '4) Ролик\n'))
+prompt_to_run = 'хуй'
+if ai == 1:
+    with io.open('myConfig.py', 'w+', encoding='utf-8') as file, io.open('configs/NeuroPryaniky.txt', 'r',
+                                                                         encoding='utf-8') as cfg_file:
         config = cfg_file.read()
         file.write(config)
     propmt_to_run = 'configs/PromptPryaniky.py'
 elif ai == 2:
-    with io.open('myConfig.py','w',encoding='utf-8') as file, io.open('configs/NeuroGTA.txt','r',encoding='utf-8') as cfg_file:
+    with io.open('myConfig.py', 'w', encoding='utf-8') as file, io.open('configs/NeuroGTA.txt', 'r',
+                                                                        encoding='utf-8') as cfg_file:
         config = cfg_file.read()
         file.write(config)
     propmt_to_run = 'configs/PromptGTA.py'
 elif ai == 3:
-    with io.open('myConfig.py','w',encoding='utf-8') as file, io.open('configs/NeuroGTA.txt','r',encoding='utf-8') as cfg_file:
+    with io.open('myConfig.py', 'w', encoding='utf-8') as file, io.open('configs/NeuroColab.txt', 'r',
+                                                                        encoding='utf-8') as cfg_file:
         config = cfg_file.read()
         file.write(config)
     propmt_to_run = 'configs/PromptColab.py'
 elif ai == 4:
-    with io.open('myConfig.py','w',encoding='utf-8') as file, io.open('configs/NeuroGTA.txt','r',encoding='utf-8') as cfg_file:
+    with io.open('myConfig.py', 'w', encoding='utf-8') as file, io.open('configs/NeuroGTA.txt', 'r',
+                                                                        encoding='utf-8') as cfg_file:
         config = cfg_file.read()
         file.write(config)
     propmt_to_run = 'configs/PromptVideo.py'
-else: print('Ps. Нужно отправить необходимую цифорку')
+else:
+    print('Ps. Нужно отправить необходимую цифорку')
+    exit()
 print('Конфиг успешно изменен')
 
 try:
